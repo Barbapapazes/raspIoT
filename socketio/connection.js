d3eca9095489e2c4491af0a9eb9446f729fdcc05
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-02-25 23:13:35 +0100

    add comment

--- socketio/connection.js
+++ socketio/connection.js
@@ -3,6 +3,7 @@ var socket = require('./index')
 exports.newConnection = function(client) {
     console.log('Client connected...');
 
+    // Sockets for index.js
     client.on('click', socket.click);
     client.on('press', socket.press);
 }
