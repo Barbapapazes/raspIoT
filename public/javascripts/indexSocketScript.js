78bed7feb681c100bb05c74f0b40912d9bea0849
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-03-21 21:12:08 +0100

    update Design

--- public/javascripts/indexSocketScript.js
+++ public/javascripts/indexSocketScript.js
@@ -6,18 +6,25 @@ var socket = io.connect('localhost:3000/');
 });*/
 
 let checkbox = document.querySelector('#chk');
+let bulb = document.querySelector('.bulb__img')
 
 checkbox.addEventListener('click', function(event) {
     if (checkbox.checked) {
         socket.emit('click', true);
+        bulb.classList.add('light')
     } else {
         socket.emit('click', false);
+        bulb.classList.remove('light')
     }
 });
 
 // wait a event from the server
 socket.on('click', function(data) {
     checkbox.checked = data;
+    if (checkbox.checked)
+        bulb.classList.add('light')
+    else
+        bulb.classList.remove('light')
 })
 
 // start the page with the gread config of each button
@@ -26,5 +33,7 @@ socket.on('newConnection', function(data) {
     if (data.state !== null) {
         console.log('Check is: ' + data.state);
         checkbox.checked = data.state;
+        if (data.state)
+            bulb.classList.add('light')
     }
 })
