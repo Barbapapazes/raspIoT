339cb026208b1eab9938d1ba3e8ff8c22904d53f
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-03-21 12:22:37 +0100

    add the sass middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/stylesheets', sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public/stylesheets'),
+    indentedSyntax: false,
+    // true = .sass and false = .scss
+    outputStyle: 'compressed',
+    sourceMap: true
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // use the socket.io
---
+++ public/javascripts/indexSwitchBtnScript.js
@@ -0,0 +1,12 @@
+let body = document.querySelector('body')
+let switchBtn = document.querySelector('.container__toggle-btn')
+
+// Change the stat of the button and the theme of the page
+switchBtn.addEventListener('click', switchTheme)
+
+function switchTheme() {
+    this.classList.toggle('active')
+    body.classList.toggle('light')
+    body.classList.toggle('dark')
+
+}
