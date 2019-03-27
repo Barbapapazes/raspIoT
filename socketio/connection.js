5eb19d3da51d182665e0204ca166f51c816608d9
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-03-27 20:19:22 +0100

    add an event which watch if the file change, to support python script

--- socketio/connection.js
+++ socketio/connection.js
@@ -9,6 +9,18 @@ exports.newConnection = function(client) {
         client.emit('newConnection', data);
     });
 
+    fs.watchFile('socketio/data.json', function(current, previous) {
+        if (current.size != previous.size) {
+            console.log('this change the file')
+            fs.readFile('socketio/data.json', 'utf-8', function(err, data) {
+                if (err) throw err;
+                client.emit('click', data);
+            });
+        } else {
+            console.log('size doesn\'t change')
+        }
+    });
+
     // Sockets for index.js
     client.on('click', socket.click);
 
