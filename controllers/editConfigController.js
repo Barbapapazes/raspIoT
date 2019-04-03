ca1eeefaefe4d01727ed46d7a61ebfbfd5ff9294
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-04-03 19:48:57 +0200

    add the page to add devices (only design)

--- controllers/editConfigController.js
+++ controllers/editConfigController.js
@@ -5,10 +5,20 @@ exports.index = function(req, res) {
 
 // Display the add menu
 exports.editConfig_add_get = function(req, res) {
-    res.send('not implemented')
+    res.render('add-devices', { title: 'RaspIoT', compagny: 'MULTI-PRISES' });
 }
 
 // Handle the post request to add device
 exports.editConfig_add_post = function(req, res) {
+    res.send('not implemented')
+}
+
+// Display the delete menu
+exports.editConfig_delete_get = function(req, res) {
+    res.send('not implemented')
+}
+
+// Handle the post request to delete device
+exports.editConfig_delete_post = function(req, res) {
     res.send('not implemented')
 }
--- routes/editConfig.js
+++ routes/editConfig.js
@@ -12,4 +12,10 @@ router.get('/add', editConfig_controller.editConfig_add_get);
 // POST request to add devices
 router.post('/add', editConfig_controller.editConfig_add_post);
 
+// GET request to delete devices
+router.get('/delete', editConfig_controller.editConfig_delete_get);
+
+// POST request to selete devices
+router.post('/delete', editConfig_controller.editConfig_delete_post);
+
 module.exports = router;
