fbe267c5f1036cbdf0fa76afb10af214e54d99b7
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-04-04 00:16:15 +0200

    add the post process to verify the data

--- controllers/editConfigController.js
+++ controllers/editConfigController.js
@@ -1,3 +1,6 @@
+const { body, check, oneOf, validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+
 // Display control panel
 exports.index = function(req, res) {
     res.render('edit-config', { title: 'RaspIoT', compagny: 'MULTI-PRISES' });
@@ -9,9 +12,48 @@ exports.editConfig_add_get = function(req, res) {
 }
 
 // Handle the post request to add device
-exports.editConfig_add_post = function(req, res) {
-    res.send('not implemented')
-}
+exports.editConfig_add_post = [
+    // Validate that the name field is not empty.
+    body('nameinput', 'Name required').isLength({ min: 1 }). /*withMessage('Must contain more than 1 letter').*/ trim(),
+
+    // Sanitize (trim and escape) the name field.
+    sanitizeBody('nameinput').trim().escape(),
+
+    // Validate that the state field have a correct value.
+    body('stateinput', 'Please define the state').isIn(['false', 'true']).withMessage('Please use predefined values'),
+
+    // Sanitize (trim and escape) the state field.
+    sanitizeBody('stateinput').trim().escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized values/error messages.
+            res.render('add-devices', {
+                title: 'RaspIoT',
+                compagny: 'MULTI-PRISES',
+                messages: errors.array()
+            })
+            return
+        } else {
+            // Data from form is valid.
+
+            console.log(req.body.nameinput)
+            console.log(req.body.stateinput)
+            res.render('add-devices', {
+                title: 'RaspIoT',
+                compagny: 'MULTI-PRISES',
+                sucess: true,
+                messages: "device correctly added"
+            })
+        }
+    }
+
+
+]
 
 // Display the delete menu
 exports.editConfig_delete_get = function(req, res) {
