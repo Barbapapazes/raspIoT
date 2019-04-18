5dc458d466cc34378604d7e25d3c3626d33c6182
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-04-18 23:29:19 +0200

    adjust

--- arduino/READfromPi.ino
+++ arduino/READfromPi.ino
@@ -10,7 +10,7 @@ void loop()
 {
   while (Serial.available())  {
     message = Serial.readString();
-    if(message == 'one') {
+    if(message == '1') {
             digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
       delay(1000);                       // wait for a second
       digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
