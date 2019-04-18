075810dc486f1a504f4ca5cf498402d3bfe41414
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-04-19 00:25:28 +0200

    able to change the state of a lamp

--- python/writeToArduino.py
+++ python/writeToArduino.py
@@ -3,4 +3,4 @@ import serial  # bibliothèque permettant la communication série
 #On écrit dans l'interface /dev/ttyUSB0 à 9600 bauds
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 #On écrit dans la console série de l'Arduino la lettre passée en paraètre lors de l'éxecution de la commande :
-ser.write(sys.argv[0].encode('utf-8'))
+ser.write(sys.argv[1].encode('ascii'))
