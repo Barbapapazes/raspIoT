fcd1ffcb514bb40b5f6d450a0083c49ec988da7f
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-04-21 20:32:47 +0200

    adjust little erros

--- arduino/READfromPi/READfromPi.ino
+++ arduino/READfromPi/READfromPi.ino
@@ -1,18 +1,34 @@
 #include <VirtualWire.h>
 #include <VWComm.h>
-int incomingInt;
-const int transmit_pin = 12; // Tx data to D12
+
+#define PINLED 8 // pin to handle the Led
+#define TX_PIN 12 // pin to use the TX
+
+int incomingByte;
+int state = LOW;
+
 VWComm vwc; // create an object to use with VWComm
-void setup()
-{
-  vw_set_tx_pin(transmit_pin);
-  vw_setup(2000); // Data Tx rate (bits per sec)
+
+void setup() {
+    Serial.begin(9600); // use for receive and transmit through the USB
+
+    vw_set_tx_pin(TX_PIN);
+    vw_setup(2000); // data tx rate (bits per sec)
+
+    pinMode(PINLED, OUTPUT);
+    digitalWrite(PINLED, state);
 }
 
-void loop()
-{
-  if(Serial.available()) {
-    incomingInt = (Serial.read() - '0');
-    vwc.sendInt(incomingInt);
-  }
+void loop () {
+    if(Serial.available()) {
+        incomingByte = (Serial.read() - '0');
+        Serial.println(incomingByte);
+        for(int i = 0; i < 3; i++)
+        {
+            vwc.sendInt(incomingByte);
+            delay(10);
+        }
+        delay(20);
+    }
 }
+
